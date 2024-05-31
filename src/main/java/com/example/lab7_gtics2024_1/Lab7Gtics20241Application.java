428b2d6b17bdb0c2700b0733fe60791f5590a460
Diego Henriquez <a20182044@pucp.edu.pe>
2024-05-30 19:22:58 -0500

    primer commit

---
+++ src/main/java/com/example/lab7_gtics2024_1/Lab7Gtics20241Application.java
@@ -0,0 +1,13 @@
+package com.example.lab7_gtics2024_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab7Gtics20241Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab7Gtics20241Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab7_gtics2024_1/Lab7Gtics20241ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab7_gtics2024_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab7Gtics20241ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
